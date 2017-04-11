4f08e0662bcb085fd07ef639138bb449aabbcb2b
FTWinston <ftwinston@gmail.com>
2017-04-11 01:09:47 +0100

    Now only drawing faces facing the camera, and filling them in.
    Still need to add symbols onto each face, transformed to fit appropriately.

--- Crew UI/game/Weapons/WeaponTarget.ts
+++ Crew UI/game/Weapons/WeaponTarget.ts
@@ -4,7 +4,7 @@
     Unknown = 3
 }
 
-class Point3D {
+class Vector3 {
     constructor(public x: number, public y: number, public z: number) { }
 
     rotateX(angle: number) {
@@ -12,7 +12,7 @@ class Point3D {
         let sina = Math.sin(angle);
         let y = this.y * cosa - this.z * sina;
         let z = this.y * sina + this.z * cosa;
-        return new Point3D(this.x, y, z);
+        return new Vector3(this.x, y, z);
     }
 
     rotateY(angle: number) {
@@ -20,7 +20,7 @@ class Point3D {
         let sina = Math.sin(angle);
         let z = this.z * cosa - this.x * sina;
         let x = this.z * sina + this.x * cosa;
-        return new Point3D(x, this.y, z)
+        return new Vector3(x, this.y, z)
     }
 
     rotateZ(angle: number) {
@@ -28,12 +28,19 @@ class Point3D {
         let sina = Math.sin(angle);
         let x = this.x * cosa - this.y * sina;
         let y = this.x * sina + this.y * cosa;
-        return new Point3D(x, y, this.z)
+        return new Vector3(x, y, this.z)
     }
 
     project(viewWidth: number, viewHeight: number, fov: number, viewDistance: number) {
-        return new Point3D(this.x * viewWidth, this.y * viewWidth, this.z);
+        return new Vector3(this.x * viewWidth, this.y * viewWidth, this.z);
     }
+    dot(other: Vector3) {
+        return this.x * other.x + this.y * other.y + this.z * other.z;
+    }
+}
+
+class Face {
+    constructor(public normal: Vector3, public vertices: [Vector3, Vector3, Vector3, Vector3]) { }
 }
 
 class WeaponTarget {
@@ -47,18 +54,28 @@ class WeaponTarget {
     }
 
     static lerpDuration: number = 1000;
-    private static vertices = [
-        new Point3D(-1, 1, -1),
-        new Point3D(1, 1, -1),
-        new Point3D(1, -1, -1),
-        new Point3D(-1, -1, -1),
-        new Point3D(-1, 1, 1),
-        new Point3D(1, 1, 1),
-        new Point3D(1, -1, 1),
-        new Point3D(-1, -1, 1)
-    ];
-    // Define the vertices that compose each of the 6 faces. Numbers are indices in vertices array
-    private static faces = [[0, 1, 2, 3], [1, 5, 6, 2], [5, 4, 7, 6], [4, 0, 3, 7], [0, 4, 5, 1], [3, 2, 6, 7]];
+    private static faces: Face[];
+
+    static init() {
+        let vertices = [
+            new Vector3(-1, 1, -1),
+            new Vector3(1, 1, -1),
+            new Vector3(1, -1, -1),
+            new Vector3(-1, -1, -1),
+            new Vector3(-1, 1, 1),
+            new Vector3(1, 1, 1),
+            new Vector3(1, -1, 1),
+            new Vector3(-1, -1, 1)
+        ];
+        WeaponTarget.faces = [
+            new Face(new Vector3(0, 0, -1), [vertices[0], vertices[1], vertices[2], vertices[3]]),
+            new Face(new Vector3(1, 0, 0),  [vertices[1], vertices[5], vertices[6], vertices[2]]),
+            new Face(new Vector3(0, 0, 1),  [vertices[5], vertices[4], vertices[7], vertices[6]]),
+            new Face(new Vector3(-1, 0, 0), [vertices[4], vertices[0], vertices[3], vertices[7]]),
+            new Face(new Vector3(0, 1, 0),  [vertices[0], vertices[4], vertices[5], vertices[1]]),
+            new Face(new Vector3(0, -1, 0), [vertices[3], vertices[2], vertices[6], vertices[7]])
+        ];
+    }
 
     id: string;
     size: number;
@@ -119,11 +136,13 @@ class WeaponTarget {
 
         ctx.save();
         if (this.status == TargetStatus.Friendly)
-            ctx.strokeStyle = ctx.fillStyle = '#00cc00';
+            ctx.fillStyle = '#00cc00';
         else if (this.status == TargetStatus.Hostile)
-            ctx.strokeStyle = ctx.fillStyle = '#cc0000';
+            ctx.fillStyle = '#cc0000';
         else
-            ctx.strokeStyle = ctx.fillStyle = '#aaaa00';
+            ctx.fillStyle = '#aaaa00';
+
+        ctx.strokeStyle = '#666666';
 
         let pitchScale = Math.cos(this.pitch);
         let rollScale = Math.cos(this.roll);
@@ -175,30 +194,39 @@ class WeaponTarget {
         this.radius = minSize * (this.size * 0.1 + 1);
     }
     private drawCube(ctx: CanvasRenderingContext2D) {
-        let rotatedVertices: Point3D[] = [];
+        let camera = new Vector3(0, 0, -1);
 
-        for (let vertex of WeaponTarget.vertices) {
-            let point = vertex
+        for (let face of WeaponTarget.faces) {
+            let dot = face.normal
+                .rotateX(this.yaw)
+                .rotateY(this.pitch)
+                .rotateZ(this.roll)
+                .dot(camera);
+
+            if (dot <= 0)
+                continue; // only draw faces visible from "above"
+
+            ctx.beginPath();
+
+            let faceVertex = 0;
+            let point = face.vertices[faceVertex]
                 .rotateX(this.yaw)
                 .rotateY(this.pitch)
                 .rotateZ(this.roll);
 
-            rotatedVertices.push(point);
-        }
-
-        for (let face of WeaponTarget.faces) {
-            ctx.beginPath();
-
-            let faceVertex = 0;
-            let point = rotatedVertices[face[faceVertex]];
             ctx.moveTo(point.x * this.radius, point.y * this.radius);
 
             for (; faceVertex < 4; faceVertex++) {
-                let point = rotatedVertices[face[faceVertex]];
+                let point = face.vertices[faceVertex]
+                    .rotateX(this.yaw)
+                    .rotateY(this.pitch)
+                    .rotateZ(this.roll);
+
                 ctx.lineTo(point.x * this.radius, point.y * this.radius);
             }
             
             ctx.closePath();
+            ctx.fill();
             ctx.stroke();
         }
     }
@@ -208,3 +236,4 @@ class WeaponTarget {
             && y >= this.renderY - r && y <= this.renderY + r;
     }
 };
+WeaponTarget.init();
