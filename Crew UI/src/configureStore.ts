6d2c4b2aa09962f7daf91730be65c5ff2d3be03e
FTWinston <ftwinston@gmail.com>
2018-11-02 14:16:21 +0000

    Corrected some redux store configuration issues

--- Crew UI/src/configureStore.ts
+++ Crew UI/src/configureStore.ts
@@ -1,35 +1,33 @@
-import { createStore, applyMiddleware, compose, combineReducers, GenericStoreEnhancer, Store, StoreEnhancerStoreCreator, ReducersMapObject } from 'redux';
+import { createStore, applyMiddleware, combineReducers, ReducersMapObject, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
-import * as StoreModule from './store';
 import { ApplicationState, reducers } from './store';
 import { History } from 'history';
 
 export default function configureStore(history: History, initialState?: ApplicationState) {
-    // Build middleware. These are functions that can process the actions before they reach the store.
-    const windowIfDefined = typeof window === 'undefined' ? null : window as any;
-    // If devTools is installed, connect to it
-    const devToolsExtension = windowIfDefined && windowIfDefined.devToolsExtension as () => GenericStoreEnhancer;
-    const createStoreWithMiddleware = compose(
-        applyMiddleware(thunk, routerMiddleware(history)),
-        devToolsExtension ? devToolsExtension() : <S>(next: StoreEnhancerStoreCreator<S>) => next
-    )(createStore);
+    const isProduction = process.env.NODE_ENV === 'production';
 
-    // Combine all reducers and instantiate the app-wide store instance
+    const middlewares = [thunk, routerMiddleware(history)];
+    const middlewareEnhancer = applyMiddleware(...middlewares)
+    const enhancers = [middlewareEnhancer];
+    
+    const composedEnhancers = isProduction
+        ? compose(...enhancers)
+        : composeWithDevTools(...enhancers);
+
     const allReducers = buildRootReducer(reducers);
-    const store = createStoreWithMiddleware(allReducers, initialState) as Store<ApplicationState>;
 
-    // Enable Webpack hot module replacement for reducers
-    if (module.hot) {
-        module.hot.accept('./store', () => {
-            const nextRootReducer = require<typeof StoreModule>('./store');
-            store.replaceReducer(buildRootReducer(nextRootReducer.reducers));
-        });
+    const store = createStore(allReducers, initialState as any, composedEnhancers as any);
+
+    // Enable Webpack hot module replacement for reducers ... most of which aren't in the store directory. And are in the same modules as their stores. Balls.
+    if (!isProduction && module.hot) {
+        module.hot.accept('./store', () => store.replaceReducer(buildRootReducer(reducers)));
     }
 
     return store;
 }
 
 function buildRootReducer(allReducers: ReducersMapObject) {
-    return combineReducers<ApplicationState>(Object.assign({}, allReducers, { routing: routerReducer }));
+    return combineReducers<ApplicationState>(Object.assign({}, allReducers, { routing: routerReducer }) as any);
 }
