b1f77c95358a93e9c6c6e6bb1073f6032e95c860
FTWinston <ftwinston@gmail.com>
2018-12-21 16:45:09 +0000

    Set up data structure / flow for new weapon system

--- Source/MakeItSo/WeaponSystem.cpp
+++ Source/MakeItSo/WeaponSystem.cpp
@@ -15,14 +15,20 @@ UWeaponSystem::UWeaponSystem()
 
 void UWeaponSystem::ResetData()
 {
-	EMPTY(currentTargetCodes);
+	selectedTargetID = 0;
+	selectedTargetingSolution = ETargetingSolution::None;
+	CLEAR(targetingSolutions);
+	ClearPuzzle();
 }
 
 bool UWeaponSystem::ReceiveCrewMessage(UIConnectionInfo *info, websocket_message *msg)
 {
 	if (MATCHES(msg, "wpn_fire"))
 	{
-		Fire();
+		// TODO: parse parameters, populate solutionSteps
+		TArray<FWeaponPuzzleData::EDirection> solutionSteps;
+
+		Fire(solutionSteps);
 	}
 	else
 		return false;
@@ -32,38 +38,256 @@ bool UWeaponSystem::ReceiveCrewMessage(UIConnectionInfo *info, websocket_message
 
 void UWeaponSystem::SendAllData_Implementation()
 {
-	// TODO: send all weapon targets?
-	SendTargetCodes();
+	SendSelectedTarget();
+	SendTargetingSolutions();
+	SendSelectedTargetingSolution();
+
+	if (selectedTargetID != 0 && selectedTargetingSolution != ETargetingSolution::None)
+		SendPuzzle();
 }
 
-#ifdef WEB_SERVER_TEST
-void UWeaponSystem::SendTargetCodes() { SendTargetCodes_Implementation(); }
-#endif
 
-void UWeaponSystem::SendTargetCodes_Implementation()
+void UWeaponSystem::SendSelectedTarget_Implementation()
 {
-	FString output = TEXT("wpn_codes");
+	FString output = TEXT("wpn_target ");
 
-	for (auto code : currentTargetCodes)
+	APPENDINT(output, selectedTargetID);
+
+	SendSystem(output);
+}
+
+void UWeaponSystem::SendTargetingSolutions_Implementation()
+{
+	FString output = TEXT("wpn_solutions");
+
+	for (auto solution : targetingSolutions)
 	{
 		output += TEXT(" ");
-		APPENDINT(output, code);
+		APPENDINT(output, (uint8)solution);
 	}
 
 	SendSystem(output);
 }
 
-
-void UWeaponSystem::OnReplicated_CurrentTargetCodes(TArray<uint16> beforeChanges)
+void UWeaponSystem::SendSelectedTargetingSolution_Implementation()
 {
-	SendTargetCodes();
+	FString output = TEXT("wpn_solution ");
+
+	APPENDINT(output, (uint8)selectedTargetingSolution);
+
+	SendSystem(output);
 }
 
-#ifdef WEB_SERVER_TEST
-void UWeaponSystem::Fire() { Fire_Implementation(); }
-#endif
-void UWeaponSystem::Fire_Implementation()
+void UWeaponSystem::SendPuzzle_Implementation()
 {
+	FString output = TEXT("wpn_puzzle ");
 
-	// TODO: actually fire
+	// TODO: send puzzle data
+
+	SendSystem(output);
+}
+
+
+void UWeaponSystem::SelectTarget_Implementation(uint16 targetID)
+{
+	selectedTargetID = targetID;
+
+	DetermineTargetingSolutions();
+
+	if (ISCLIENT())
+	{
+		SendSelectedTarget();
+		SendTargetingSolutions();
+	}
+}
+
+void UWeaponSystem::SelectTargetingSolution_Implementation(ETargetingSolution solution)
+{
+	if (selectedTargetID == 0 || !SETCONTAINS(targetingSolutions, solution))
+		return; // invalid value, or invalid time
+
+	selectedTargetingSolution = solution;
+	if (ISCLIENT())
+		SendSelectedTargetingSolution();
+
+	if (solution != ETargetingSolution::None)
+	{
+		GeneratePuzzle();
+		if (ISCLIENT())
+			SendPuzzle();
+	}
+}
+
+void UWeaponSystem::Fire_Implementation(TArray<FWeaponPuzzleData::EDirection> puzzleSolution)
+{
+	if (selectedTargetID == 0 || selectedTargetingSolution == 0 || targetingPuzzle.width == 0)
+		return;
+
+	if (!IsValidSolution(puzzleSolution))
+		return;
+
+	auto targetSystem = GetSystemForSolution(selectedTargetingSolution);
+	auto damage = GetDamageForSolution(selectedTargetingSolution);
+
+	// TODO: actually fire ... deal damage to targetSystem
+
+	ClearPuzzle();
+}
+
+void UWeaponSystem::DetermineTargetingSolutions()
+{
+	CLEAR(targetingSolutions);
+
+	if (selectedTargetID = 0)
+		return;
+
+	// TODO: decide available targeting solutions based on target type and sensor data
+	// TODO: in particular, sensors needs to make vulnerabilities available here 
+	SETADD(targetingSolutions, ETargetingSolution::Engines);
+	SETADD(targetingSolutions, ETargetingSolution::Warp);
+	SETADD(targetingSolutions, ETargetingSolution::Weapons);
+	SETADD(targetingSolutions, ETargetingSolution::Sensors);
+	SETADD(targetingSolutions, ETargetingSolution::PowerManagement);
+	SETADD(targetingSolutions, ETargetingSolution::DamageControl);
+	SETADD(targetingSolutions, ETargetingSolution::Communications);
+}
+
+uint8 UWeaponSystem::DeterminePuzzleSize()
+{
+	uint8 puzzleSize;
+
+	if (selectedTargetingSolution == ETargetingSolution::None)
+		puzzleSize = 0;
+	else if (selectedTargetingSolution == ETargetingSolution::Misc)
+		puzzleSize = 3;
+	else if (selectedTargetingSolution == ETargetingSolution::MiscVulnerability)
+		puzzleSize = 7;
+	else if (selectedTargetingSolution >= ETargetingSolution::MIN_STANDARD_SYSTEM && selectedTargetingSolution <= ETargetingSolution::MAX_STANDARD_SYSTEM)
+		puzzleSize = 6;
+	else if (selectedTargetingSolution >= ETargetingSolution::MIN_SYSTEM_VULNERABILITY && selectedTargetingSolution <= ETargetingSolution::MAX_SYSTEM_VULNERABILITY)
+		puzzleSize = 6;
+	else
+		puzzleSize = 2;
+
+	auto health = GetHealthLevel();
+	if (health == 0)
+		puzzleSize = 0; // cannot fire
+	else if (health < 10)
+		puzzleSize += 6;
+	else if (health < 30)
+		puzzleSize += 5;
+	else if (health < 50)
+		puzzleSize += 4;
+	else if (health < 70)
+		puzzleSize += 3;
+	else if (health < 90)
+		puzzleSize += 2;
+	else if (health < 100)
+		puzzleSize += 1;
+
+	return puzzleSize;
+}
+
+void UWeaponSystem::GeneratePuzzle()
+{
+	targetingPuzzle.width = DeterminePuzzleSize();
+	CLEAR(targetingPuzzle.cells);
+
+	// TODO: generate a proper puzzle ... this is a placeholder
+	uint8 numCells = targetingPuzzle.width * targetingPuzzle.width;
+	targetingPuzzle.startCell = FMath::RandRange(0, numCells - 1);
+	for (uint8 i = 0; i < numCells; i++)
+		SETADD(targetingPuzzle.cells, FMath::RandRange(0, 10) == 0);
+}
+
+void UWeaponSystem::ClearPuzzle()
+{
+	targetingPuzzle.width = 0;
+	CLEAR(targetingPuzzle.cells);
+	targetingPuzzle.startCell = 0;
+}
+
+bool UWeaponSystem::IsValidSolution(TArray<FWeaponPuzzleData::EDirection> puzzleSolution)
+{
+	if (targetingPuzzle.width == 0)
+		return false; // Cannot fire if we have no targeting solution
+
+	// TODO: solve this
+	return false;
+}
+
+UShipSystem::ESystem UWeaponSystem::GetSystemForSolution(ETargetingSolution solution)
+{
+	switch (solution)
+	{
+	case Misc:
+	case MiscVulnerability:
+		switch (FMath::RandRange(1, 7))
+		{
+		case 1:
+			return UShipSystem::Helm;
+		case 2:
+			return UShipSystem::Warp;
+		case 3:
+			return UShipSystem::Weapons;
+		case 4:
+			return UShipSystem::Sensors;
+		case 5:
+			return UShipSystem::PowerManagement;
+		case 6:
+			return UShipSystem::DamageControl;
+		case 7:
+			return UShipSystem::Communications;
+		default:
+			return UShipSystem::ESystem::None;
+		}
+
+	case Engines:
+	case EngineVulnerability:
+		return UShipSystem::Helm;
+
+	case Warp:
+	case WarpVulnerability:
+		return UShipSystem::Warp;
+
+	case Weapons:
+	case WeaponVulnerability:
+		return UShipSystem::Weapons;
+
+	case Sensors:
+	case SensorVulnerability:
+		return UShipSystem::Sensors;
+
+	case PowerManagement:
+	case PowerVulnerability:
+		return UShipSystem::PowerManagement;
+
+	case DamageControl:
+	case DamageControlVulnerability:
+		return UShipSystem::DamageControl;
+
+	case Communications:
+		return UShipSystem::Communications;
+
+	default:
+		// TODO: determine how to handle targeting non-ships. Still use "misc" solution, but don't pick an actual ship system?
+		return UShipSystem::ESystem::None;
+	}
+}
+
+uint8 UWeaponSystem::GetDamageForSolution(ETargetingSolution solution)
+{
+	uint8 damage = 0; 
+
+	if (solution == ETargetingSolution::Misc)
+		damage = FMath::RandRange(15, 30);
+	else if (solution == ETargetingSolution::MiscVulnerability)
+		damage = FMath::RandRange(35, 55);
+	else if (solution >= ETargetingSolution::MIN_STANDARD_SYSTEM && solution <= ETargetingSolution::MAX_STANDARD_SYSTEM)
+		damage = FMath::RandRange(20, 40);
+	else if (solution >= ETargetingSolution::MIN_SYSTEM_VULNERABILITY && solution <= ETargetingSolution::MAX_SYSTEM_VULNERABILITY)
+		damage = FMath::RandRange(55, 75);
+
+	// scale damage should by system power
+	return damage * GetPowerLevel() / 100;
 }
--- Source/MakeItSo/WeaponSystem.h
+++ Source/MakeItSo/WeaponSystem.h
@@ -7,31 +7,153 @@
 #include "ShipSystem.h"
 #include "WeaponSystem.Generated.h"
 
+USTRUCT()
+struct FWeaponPuzzleData
+{
+	enum EDirection : uint8
+	{
+		Up = 1,
+		Down = 2,
+		Left = 3,
+		Right = 4,
+	};
+
+	GENERATED_BODY()
+
+	UPROPERTY(Replicated)
+	uint8 width;
+
+	UPROPERTY(Replicated)
+	TArray<bool> cells;
+
+	UPROPERTY(Replicated)
+	uint8 startCell;
+};
+
 UCLASS()
 class MAKEITSO_API UWeaponSystem : public UShipSystem
 {
+	enum ETargetingSolution : uint8
+	{
+		None = 0,
+
+		Misc,
+
+		Engines,
+		Warp,
+		Weapons,
+		Sensors,
+		PowerManagement,
+		DamageControl,
+		Communications,
+
+		MiscVulnerability,
+		EngineVulnerability,
+		WarpVulnerability,
+		WeaponVulnerability,
+		SensorVulnerability,
+		PowerVulnerability,
+		DamageControlVulnerability,
+
+		MIN_STANDARD_SYSTEM = Engines,
+		MAX_STANDARD_SYSTEM = Communications,
+		MIN_SYSTEM_VULNERABILITY = EngineVulnerability,
+		MAX_SYSTEM_VULNERABILITY = DamageControlVulnerability
+	};
+
 	GENERATED_BODY()
 public:
 	UWeaponSystem();
 	virtual void ResetData() override;
 	virtual bool ReceiveCrewMessage(UIConnectionInfo *info, websocket_message *msg) override;
 	virtual void SendAllData_Implementation() override;
+
 protected:
 	virtual UShipSystem::ESystem GetSystem() override { return UShipSystem::ESystem::Weapons; }
+
 private:
-	UPROPERTY(Replicated, ReplicatedUsing = OnReplicated_CurrentTargetCodes)
-	TArray<uint16> currentTargetCodes;
-	void OnReplicated_CurrentTargetCodes(TArray<uint16> beforeChange);
+	void DetermineTargetingSolutions();
+	void ClearPuzzle();
+	uint8 DeterminePuzzleSize();
+	void GeneratePuzzle();
+	bool IsValidSolution(TArray<FWeaponPuzzleData::EDirection> puzzleSolution);
+	UShipSystem::ESystem GetSystemForSolution(ETargetingSolution solution);
+	uint8 GetDamageForSolution(ETargetingSolution solution);
+
+
+	UPROPERTY(Replicated, ReplicatedUsing = OnReplicated_SelectedTargetID)
+	uint16 selectedTargetID;
+	void OnReplicated_SelectedTargetID(uint16 beforeChange) { SendSelectedTarget(); }
+
+	UPROPERTY(Replicated, ReplicatedUsing = OnReplicated_TargetingSolutions)
+	TSet<ETargetingSolution> targetingSolutions;
+	void OnReplicated_TargetingSolutions(TSet<ETargetingSolution> beforeChange) { SendTargetingSolutions(); }
+
+	UPROPERTY(Replicated, ReplicatedUsing = OnReplicated_SelectedTargetingSolution)
+	ETargetingSolution selectedTargetingSolution;
+	void OnReplicated_SelectedTargetingSolution(ETargetingSolution beforeChange) { SendSelectedTargetingSolution(); }
+
+	UPROPERTY(Replicated, ReplicatedUsing = OnReplicated_TargetingPuzzle)
+	FWeaponPuzzleData targetingPuzzle;
+	void OnReplicated_TargetingPuzzle(FWeaponPuzzleData beforeChange) { SendPuzzle(); }
+
 
 	UFUNCTION(Client, Reliable)
-	void SendTargetCodes();
+	void SendSelectedTarget()
 #ifdef WEB_SERVER_TEST
-	void SendTargetCodes_Implementation();
+	{ SendSelectedTarget_Implementation(); }
+	void SendSelectedTarget_Implementation();
 #endif
+	;
+
+	UFUNCTION(Client, Reliable)
+	void SendTargetingSolutions()
+	{ SendTargetingSolutions_Implementation(); }
+#ifdef WEB_SERVER_TEST
+	void SendTargetingSolutions_Implementation();
+#endif
+	;
+
+	UFUNCTION(Client, Reliable)
+	void SendSelectedTargetingSolution()
+#ifdef WEB_SERVER_TEST
+	{ SendSelectedTargetingSolution_Implementation(); }
+	void SendSelectedTargetingSolution_Implementation();
+#endif
+	;
+
+	UFUNCTION(Client, Reliable)
+	void SendPuzzle()
+#ifdef WEB_SERVER_TEST
+	{ SendPuzzle_Implementation(); }
+	void SendPuzzle_Implementation();
+#endif
+	;
+
 
 	UFUNCTION(Server, Reliable)
-	void Fire();
+	void SelectTarget(uint16 targetID)
 #ifdef WEB_SERVER_TEST
-	void Fire_Implementation();
+	{ SelectTarget_Implementation(targetID); }
+	void SelectTarget_Implementation(uint16 targetID);
 #endif
+	;
+
+	UFUNCTION(Server, Reliable)
+	void SelectTargetingSolution(ETargetingSolution solution)
+#ifdef WEB_SERVER_TEST
+	{
+		SelectTargetingSolution_Implementation(solution);
+	}
+	void SelectTargetingSolution_Implementation(ETargetingSolution solution);
+#endif
+	;
+
+	UFUNCTION(Server, Reliable)
+	void Fire(TArray<FWeaponPuzzleData::EDirection> puzzleSolution)
+#ifdef WEB_SERVER_TEST
+	{ Fire_Implementation(puzzleSolution); }
+	void Fire_Implementation(TArray<FWeaponPuzzleData::EDirection> puzzleSolution);
+#endif
+	;
 };
