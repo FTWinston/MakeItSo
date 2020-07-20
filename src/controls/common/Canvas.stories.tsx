b8a1bd322d19cdc44c7cffc7d3eccc8e65dc2fe2
FTWinston <ftwinston@gmail.com>
2020-07-21 00:04:09 +0100

    Added canvas component

---
+++ src/controls/common/Canvas.stories.tsx
@@ -0,0 +1,73 @@
+import React, { useRef, useMemo, useLayoutEffect, useState } from 'react';
+import { action } from '@storybook/addon-actions';
+import { Theme } from '../../style/theme';
+import { Canvas } from './Canvas';
+import { makeStyles } from '@material-ui/core';
+
+export default { title: 'Common/Canvas' };
+
+
+const defaultBounds = new DOMRect(0, 0, 1, 1);
+
+const useStyles = makeStyles(theme => ({
+    canvas: {
+        width: '100vw',
+        height: '100vh',
+    },
+}));
+    
+const Simple = () => {
+    const canvas = useRef<HTMLCanvasElement>(null);
+
+    const classes = useStyles();
+    
+    const context = useMemo(
+        () => canvas.current
+            ? canvas.current.getContext('2d')
+            : null,
+        [canvas.current]
+    );
+
+    const [bounds, setBounds] = useState<DOMRect>(defaultBounds);
+
+    const redraw = () => {
+        if (!context) {
+            return;
+        }
+
+        context.translate(-bounds.x, -bounds.y);
+        context.clearRect(bounds.x, bounds.y, bounds.width, bounds.height);
+
+        draw(context, bounds);
+        context.translate(bounds.x, bounds.y);
+    }
+
+    useLayoutEffect(
+        () => redraw(),
+        [context, bounds]
+    );
+
+    return (
+        <Canvas
+            ref={canvas}
+            className={classes.canvas}
+            boundsChanged={setBounds}
+        />
+    )
+}
+
+export const simple = () => (
+    <Theme>
+        <Simple />
+    </Theme>
+);
+
+function draw(
+    ctx: CanvasRenderingContext2D,
+    viewBounds: DOMRect,
+) {
+    console.log('viewBounds', viewBounds);
+    ctx.fillStyle = '#090';
+    ctx.fillRect(viewBounds.x, viewBounds.y, viewBounds.width * 0.5, viewBounds.height * 0.5);
+    ctx.fillRect(viewBounds.x + viewBounds.width * 0.5, viewBounds.y + viewBounds.height * 0.5, viewBounds.width * 0.5, viewBounds.height * 0.5);
+}
---
+++ src/controls/common/Canvas.tsx
@@ -0,0 +1,124 @@
+import React, { useMemo, useRef, useEffect, forwardRef, useState, CSSProperties } from 'react';
+import { makeStyles } from '@material-ui/core';
+import ResizeObserver from 'resize-observer-polyfill';
+
+interface Props {
+    className?: string;
+    boundsChanged: (bounds: DOMRect) => void;
+    onClick?: (x: number, y: number) => void;
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        position: 'relative',
+    },
+    display: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+    },
+}));
+
+export const Canvas = forwardRef<HTMLCanvasElement, Props>((props, ref) => {
+    const classes = useStyles();
+
+    const outerRef = useRef<HTMLDivElement>(null);
+    let canvasRef = useRef<HTMLCanvasElement>(null);
+
+    if (ref) {
+        canvasRef = ref as React.RefObject<HTMLCanvasElement>;
+    }
+
+    // TODO: replicate this for onMouseDown etc?
+    let onClick = useMemo(
+        () => {
+            const { onClick } = props;
+            if (!onClick || !outerRef.current) {
+                return undefined;
+            }
+
+            const outer = outerRef.current;
+
+            return (e: React.MouseEvent<HTMLCanvasElement>) => {
+                const { x, y } = resolvePosition(e, outer);
+
+                onClick(x, y);
+            }
+        },
+        [props.onClick]
+    );
+    if (!props.onClick) {
+        onClick = undefined;
+    }
+
+    const [displaySizeStyle, setDisplaySizeStyle] = useState<CSSProperties>();
+
+    useEffect(
+        () => {
+            const display = canvasRef.current;
+
+            if (!display) {
+                return;
+            }
+
+            const updateCanvasSize = () => {
+                const displayWidth = outerRef.current!.clientWidth;
+                const displayHeight = outerRef.current!.clientHeight;
+
+                if (display.width !== displayWidth || display.height !== displayHeight) {
+                    display.width = displayWidth;
+                    display.height = displayHeight;
+
+                    setDisplaySizeStyle({
+                        width: `${displayWidth}px`,
+                        height: `${displayHeight}px`,
+                    });
+
+                    const bounds = new DOMRect(
+                        0,
+                        0,
+                        displayWidth,
+                        displayHeight
+                    );
+                    
+                    props.boundsChanged(bounds);
+                }
+            };
+
+            updateCanvasSize();
+
+            const resizeObserver = new ResizeObserver(() => updateCanvasSize());
+            resizeObserver.observe(outerRef.current!);
+
+            return () => resizeObserver.disconnect();
+        },
+        [canvasRef.current, props.boundsChanged]
+    );
+
+    const rootClasses = props.className
+        ? `${classes.root} ${props.className}`
+        : classes.root;
+
+    return (
+        <div className={rootClasses} ref={outerRef}>
+            <canvas
+                className={classes.display}
+                style={displaySizeStyle}
+                ref={ref}
+                onClick={onClick}
+            />
+        </div>
+    );
+})
+
+function resolvePosition(e: React.MouseEvent<HTMLCanvasElement, MouseEvent>, outer: HTMLDivElement) {
+    const scroller = e.currentTarget;
+    const outerBounds = outer.getBoundingClientRect();
+    
+    return {
+        x: e.clientX - outerBounds.left + scroller.scrollLeft,
+        y: e.clientY - outerBounds.top + scroller.scrollTop,
+    };
+}
