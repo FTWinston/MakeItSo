2f16b749795e18341b4a97a446f9df72142ea31c
FTWinston <ftwinston@gmail.com>
2020-07-26 11:13:48 +0100

    Canvas story now uses theme color

--- src/controls/common/Canvas.stories.tsx
+++ src/controls/common/Canvas.stories.tsx
@@ -1,7 +1,7 @@
-import React, { useRef, useMemo, useLayoutEffect, useState } from 'react';
-import { Theme } from '../../style/theme';
+import React, { useRef, useCallback } from 'react';
+import { Theme as ThemeProvider } from '../../style/theme';
 import { Canvas } from './Canvas';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, useTheme, Theme } from '@material-ui/core';
 
 export default { title: 'Common/Canvas' };
 
@@ -16,28 +16,35 @@ const Simple = () => {
     const canvas = useRef<HTMLCanvasElement>(null);
 
     const classes = useStyles();
+
+    const theme = useTheme();
+
+    const drawThemed = useCallback(
+        (ctx: CanvasRenderingContext2D, bounds: DOMRect) => draw(ctx, bounds, theme),
+        [theme]
+    );
     
     return (
         <Canvas
             ref={canvas}
             className={classes.canvas}
-            draw={draw}
+            draw={drawThemed}
         />
     )
 }
 
 export const simple = () => (
-    <Theme>
+    <ThemeProvider>
         <Simple />
-    </Theme>
+    </ThemeProvider>
 );
 
 function draw(
     ctx: CanvasRenderingContext2D,
     viewBounds: DOMRect,
+    theme: Theme
 ) {
-    console.log('viewBounds', viewBounds);
-    ctx.fillStyle = '#090';
+    ctx.fillStyle = theme.palette.primary.main;
     ctx.fillRect(viewBounds.x, viewBounds.y, viewBounds.width * 0.5, viewBounds.height * 0.5);
     ctx.fillRect(viewBounds.x + viewBounds.width * 0.5, viewBounds.y + viewBounds.height * 0.5, viewBounds.width * 0.5, viewBounds.height * 0.5);
 }
