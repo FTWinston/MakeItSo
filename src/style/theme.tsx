2cff69d9af10fc099a286532ef79c1457b12bbd3
FTWinston <ftwinston@gmail.com>
2020-07-28 18:18:32 +0100

    Tweaked theme colors

--- src/style/theme.tsx
+++ src/style/theme.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { deepOrange, orange } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({
     palette: {
@@ -9,7 +10,15 @@ const theme = createMuiTheme({
             contrastText: '#000',
         },
         secondary: {
-            main: '#E08138',
+            light: orange[300],
+            main: orange[400],
+            dark: orange[500],
+            contrastText: '#000',
+        },
+        warning: {
+            main: deepOrange[400],
+            light: deepOrange[300],
+            dark: deepOrange[600],
             contrastText: '#000',
         }
     },
