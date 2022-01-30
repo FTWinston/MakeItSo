fe3460dd918458a0e61619ce7deea003ef2b194b
FTWinston <ftwinston@gmail.com>
2022-01-30 22:41:38 +0000

    Engineering system tiles now use svg for their health indicator

--- src/features/engineering/features/SystemTiles/components/SystemTile.tsx
+++ src/features/engineering/features/SystemTiles/components/SystemTile.tsx
@@ -16,31 +16,30 @@ const Root = styled('div')(({ theme }) => ({
     height: '6em',
 }));
 
-const OuterCircle = styled('div')({
+const HealthSvg = styled('svg')({
     zIndex: 1,
     position: 'absolute',
-    borderRadius: '50%',
     top: '0.1em; right: 0.1em',
     bottom: '0.1em; left: 0.1em',
-    overflow: 'hidden',
 });
 
-const InnerCircle = styled('div')(({ theme }) => ({
-    position: 'absolute',
-    borderRadius: '50%',
-    top: '0.4em; right: 0.4em',
-    bottom: '0.4em; left: 0.4em',
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: 'inset rgba(34, 36, 38, 0.15) 0px 0px 3px 2px',
-}));
+const ellipseRx = 38;
+const ellipseRy = 28;
+const ellipsePerimeter = 208.5638; // See https://www.mathsisfun.com/geometry/ellipse-perimeter.html
 
-const Segment = styled('div')({
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    width: '100%',
-    height: '100%',
-    transformOrigin: '0 0',
+const HealthEllipse = styled('ellipse')<{ health: number }>(props => {
+    const healthScale = props.health / 100;
+    const healthByte = Math.round(healthScale * 255);
+    const color = `rgb(${255 - healthByte}, ${healthByte}, 0)`;
+    const fillDistance = healthScale * ellipsePerimeter;
+
+    return {
+        stroke: color,
+        fill: 'none',
+        strokeDasharray: `${fillDistance}, ${ellipsePerimeter - fillDistance}`,
+        strokeDashoffset: ellipsePerimeter * 0.25,
+        transition: 'stroke-dasharray 300ms ease-in-out',
+    };
 });
 
 const Name = styled(Typography)({
@@ -59,65 +58,28 @@ const Effects = styled(EffectIndicators)({
 });
 
 export const SystemTile: React.FC<TileDisplayInfo> = (props) => {
-    const constrainHealth = Math.max(0, Math.min(100, props.health));
-    const healthInt = Math.round(constrainHealth);
-    const healthByte = props.health * 2.55;
-    const healthAngle = props.health * 3.6;
-    const healthColor = `rgb(${255 - healthByte}, ${healthByte}, 0)`;
-
-    const skew1 = healthAngle <= 90
-        ? 90 - healthAngle
-        : 0;
-
-    const skew2 = healthAngle > 90 && healthAngle <= 180
-        ? 180 - healthAngle
-        : healthAngle > 180
-            ? 0
-            : 90;
-
-    const skew3 = healthAngle > 180 && healthAngle <= 270
-        ? 270 - healthAngle
-        : healthAngle > 270
-            ? 0
-            : 90;
-        
-    const skew4 = healthAngle > 270
-        ? 360 - healthAngle
-        : 90;
-
-    const segment1: React.CSSProperties = {
-        transform: `rotate(0deg) skew(${skew1}deg)`,
-        backgroundColor: healthColor,
-    };
-    const segment2: React.CSSProperties = {
-        transform: `rotate(90deg) skew(${skew2}deg)`,
-        backgroundColor: healthColor,
-    };
-    const segment3: React.CSSProperties = {
-        transform: `rotate(180deg) skew(${skew3}deg)`,
-        backgroundColor: healthColor,
-    };
-    const segment4: React.CSSProperties = {
-        transform: `rotate(270deg) skew(${skew4}deg)`,
-        backgroundColor: healthColor,
-    };
+    const constrainedHealth = Math.max(0, Math.min(100, props.health));
 
     return (
         <Root role="group">
-            <OuterCircle role="presentation">
-                <Segment style={segment1} />
-                <Segment style={segment2} />
-                <Segment style={segment3} />
-                <Segment style={segment4} />
-                <InnerCircle />
-            </OuterCircle>
+            <HealthSvg
+                role="presentation"
+                viewBox="0 0 80 60"
+            >
+                <HealthEllipse
+                    cx="40" cy="30"
+                    rx={ellipseRx} ry={ellipseRy}
+                    health={constrainedHealth}
+                    strokeWidth={3.6}
+                />
+            </HealthSvg>
 
             <Name variant="h3">
                 {props.name}
             </Name>
 
             <Health aria-label="health">
-                <span role="presentation">H</span> {healthInt}%
+                <span role="presentation">H</span> {Math.round(constrainedHealth)}%
             </Health>
 
             <Effects effects={props.effects} />
