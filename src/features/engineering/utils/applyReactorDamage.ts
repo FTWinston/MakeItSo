c193fc86b353bdd115514addbd287c8722d9e156
FTWinston <ftwinston@gmail.com>
2023-01-11 16:22:31 +0000

    Reactor effect interval adjusts based on health

--- src/features/engineering/utils/applyReactorDamage.ts
+++ src/features/engineering/utils/applyReactorDamage.ts
@@ -4,17 +4,38 @@ import { durationToTicks } from 'src/utils/timeSpans';
 import { SystemStatusEffectType } from '../types/SystemStatusEffect';
 import { applySingleEffect, getRandomSystem } from './systemActions';
 
+function scaleNumber(input: number, inputStart: number, inputEnd: number, outputStart: number, outputEnd: number) {
+    const fraction = (input - inputStart) / (inputEnd - inputStart);
+    return outputStart + fraction * (outputEnd - outputStart);
+}
+
 function getReactorDamageEffectApplicationInterval(reactorHealth: number) {
+    let interval: number;
+
     if (reactorHealth > 80) {
         return;
     }
+    else if (reactorHealth > 50) {
+        // Apply every 45 seconds at 80% health
+        // Apply every 10 seconds at 50% health
+        interval = scaleNumber(reactorHealth, 50, 80, 10, 45);
+    }
+    else if (reactorHealth > 15) {
+        // Apply every 10 seconds at 50% health
+        // Apply every 5 seconds at 15% health
+        interval = scaleNumber(reactorHealth, 15, 50, 5, 10);
+    }
+    else if (reactorHealth > 1) {
+        // Apply every 5 seconds at 15% health
+        // Apply every 1 seconds at 1% health
+        interval = scaleNumber(reactorHealth, 1, 15, 1, 5);
+    }
+    else {
+        // Apply every 1 seconds at 1% health
+        interval = 1;
+    }
     
-    // TODO: calculate this based on health
-
-    // Apply every 45 seconds at 80% health
-    // Apply every 5 seconds at 10% health
-    // Apply every 2 seconds at 1% health
-    return durationToTicks(45);
+    return durationToTicks(interval);
 }
 
 export function applyReactorDamage(state: ShipState, currentTime: number) {
