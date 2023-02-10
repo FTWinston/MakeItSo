3e051547c8919a0d2f37e4c8821b31f6cc82a30e
FTWinston <ftwinston@gmail.com>
2023-02-10 23:05:34 +0000

    Fixed Fab sizing

--- src/lib/mui/Fab.tsx
+++ src/lib/mui/Fab.tsx
@@ -2,5 +2,7 @@ import MuiFab from '@mui/material/Fab';
 import { styled } from '@mui/material/styles';
 
 export const Fab = styled(MuiFab)({  
-    
+    width: '3.5em',
+    height: '3.5em',
+    minHeight: '2.25em',
 });
