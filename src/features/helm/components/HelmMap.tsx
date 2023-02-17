627976d927e025090701295d58d8f22824276828
FTWinston <ftwinston@gmail.com>
2023-02-17 22:57:38 +0000

    Lock camera onto ship in manuever mode

--- src/features/helm/components/HelmMap.tsx
+++ src/features/helm/components/HelmMap.tsx
@@ -1,7 +1,7 @@
-import { useMemo, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { Position } from 'src/types/Position';
 import { distanceSq, Vector2D } from 'src/types/Vector2D';
-import { getPositionValue } from 'src/types/Keyframes';
+import { getPositionValue, getVectorValue } from 'src/types/Keyframes';
 import { SpaceMap } from 'src/features/spacemap';
 import { useTheme } from 'src/lib/mui';
 import { drawDestination } from '../utils/drawDestination';
@@ -14,7 +14,7 @@ import { StopAndFocus } from './StopAndFocus';
 import { Mode } from './ModeToggle';
 import { useHasChanged } from 'src/hooks/useHasChanged';
 import { useShipVisibility } from '../hooks/useShipVisibility';
-import { getEndPosition } from '../utils/getEndPosition';
+import { getManeuverStartPosition } from '../utils/getManeuverStartPosition';
 
 interface Props {
     mode: Mode;
@@ -52,21 +52,43 @@ export const HelmMap: React.FC<Props> = props => {
     } = useHelmMapInteractions(canvas, motion, !inManeuverMode, inManeuverMode ? undefined : setDestination, getInitialCenter);
 
     // In maneuver mode, center the view on the end of the ship's planned motion.
-    let autoFocusPoint = inManeuverMode
-        ? getEndPosition(motion, maneuvers)
+    const currentTime = getTime();
+
+    const previewStartPosition = inManeuverMode
+        ? getManeuverStartPosition(motion, maneuvers, currentTime)
         : undefined;
 
+    const autoFocusPoint = inManeuverMode
+        ? maneuvers.length > 0 && previewStartPosition
+            ? previewStartPosition.val
+            : undefined//getPositionValue(motion, currentTime)
+        : undefined;
+
+    // In maneuver mode, with no maneuvers, the camera should lock to the ship.
+    useEffect(() => {
+        if (!inManeuverMode || maneuvers.length > 0) {
+            return;
+        }
+
+        const interval = setInterval(() => {
+            const shipPos = getVectorValue(motion, getTime());
+            setViewCenter(shipPos);
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, [inManeuverMode, maneuvers, motion])
+
     const maneuverSets = useMemo(() => {
-        if (!previewManeuver || !autoFocusPoint) {
+        if (!previewManeuver || !previewStartPosition) {
             return [maneuvers];
         }
 
-        const actualPreviewManeuever = getManeuver(previewManeuver, autoFocusPoint);
+        const actualPreviewManeuever = getManeuver(previewManeuver, previewStartPosition);
         return [maneuvers, [actualPreviewManeuever]];
-    }, [previewManeuver, maneuvers, autoFocusPoint])
+    }, [previewManeuver, maneuvers, previewStartPosition])
     
     if (useHasChanged(autoFocusPoint) && autoFocusPoint) {
-        setViewCenter(autoFocusPoint.val);
+        setViewCenter(autoFocusPoint);
     }
 
     const shipVisible = useShipVisibility(canvas, motion, getViewCenter, getCellRadius);
@@ -83,13 +105,15 @@ export const HelmMap: React.FC<Props> = props => {
             }
         }
 
-        const currentTime = getTime();
+        let currentTime = getTime();
         
         for (const maneuverSet of maneuverSets) {
             for (let i=0; i < maneuverSet.length; i++) {
                 const maneuver = maneuverSet[i];
                 drawManeuver(ctx, maneuver.motion, maneuver.minPower, i === maneuverSet.length - 1, currentTime);
             }
+
+            currentTime = 0; // Ensure that "preview" maneuvers are always fully rendered, as if they're in the future. This is a hack on account of them sometimes being in the past.
         }
     };
 
