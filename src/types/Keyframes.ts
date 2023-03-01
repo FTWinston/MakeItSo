1ef7bf7406ae9848337204b8e8104414ab9ad996
FTWinston <ftwinston@gmail.com>
2023-03-01 17:14:18 +0000

    No longer caching keyframe curve parameters.

--- src/types/Keyframes.ts
+++ src/types/Keyframes.ts
@@ -2,19 +2,10 @@ import { clampAngle, Vector2D } from './Vector2D';
 import { Position } from './Position';
 import { getTime } from 'src/utils/timeSpans';
 
-interface CurveParameters<T> {
-    a: T;
-    b: T;
-    c: T;
-}
-
 export interface Keyframe<T> {
     time: number;
     val: T;
-    curve?: CurveParameters<T>;
 }
-
-type ResolvedKeyframe<T> = Required<Keyframe<T>>;
 
 export type Keyframes<T> = Keyframe<T>[];
 
@@ -54,7 +45,7 @@ function distance(from: number, to: number) {
 
 // Value resolving layer 1, part 1: determining curve parameters for a keypoint, based on values of surrounding keypoints.
 
-function determineCurveParameters(val0: number | undefined, val1: number, val2: number, val3: number | undefined): CurveParameters<number> {
+function determineCurveParameters(val0: number | undefined, val1: number, val2: number, val3: number | undefined): [number, number, number] {
     if (val0 === undefined) {
         val0 = val1;
     }
@@ -71,11 +62,11 @@ function determineCurveParameters(val0: number | undefined, val1: number, val2: 
     const m1 = val2 - val1 + t12 * ((val1 - val0) / t01 - (val2 - val0) / (t01 + t12));
     const m2 = val2 - val1 + t12 * ((val3 - val2) / t23 - (val3 - val1) / (t12 + t23));
 
-    return {
-        a: 2 * (val1 - val2) + m1 + m2,
-        b: -3 * (val1 - val2) - m1 - m1 - m2,
-        c: m1,
-    };
+    return [
+        2 * (val1 - val2) + m1 + m2,
+        -3 * (val1 - val2) - m1 - m1 - m2,
+        m1,
+    ];
 }
 
 function determineCurveParametersForAngle(angle0: number | undefined, angle1: number, angle2: number, angle3: number | undefined) {
@@ -107,39 +98,6 @@ function determineCurveParametersForAngle(angle0: number | undefined, angle1: nu
     return determineCurveParameters(angle0, angle1, angle2, angle3);
 }
 
-function determineCurveForObject<T extends object>(
-    keyframes: Keyframes<T>,
-    frame1: Keyframe<T>,
-    frame2: Keyframe<T>,
-    index2: number,
-    keys: ReadonlyArray<keyof T & string>
-): CurveParameters<T> {
-    const frame0 = keyframes[index2 - 2];
-    const frame3 = keyframes[index2 + 1];
-
-    const a: Partial<Record<keyof T, number>> = {};
-    const b: Partial<Record<keyof T, number>> = {};
-    const c: Partial<Record<keyof T, number>> = {};
-
-    const val0 = (frame0?.val ?? {}) as Record<keyof T, number>;
-    const val1 = frame1.val as Record<keyof T, number>;
-    const val2 = frame2.val as Record<keyof T, number>;
-    const val3 = (frame3?.val ?? {})  as Record<keyof T, number>;
-
-    for (const key of keys) {
-        const keyCurve = determineCurveParameters(val0[key], val1[key], val2[key], val3[key]);
-
-        a[key] = keyCurve.a;
-        b[key] = keyCurve.b;
-        c[key] = keyCurve.c;
-    }
-
-    return { a, b, c } as CurveParameters<T>;
-}
-
-
-// Value rendering layer 1, part 2: Resolving values based on last keypoint's parameters.
-
 function resolveNumberValue(a: number, b: number, c: number, d: number, fraction: number) {
     return a * fraction * fraction * fraction
          + b * fraction * fraction
@@ -152,27 +110,6 @@ function resolveAngleValue(a: number, b: number, c: number, d: number, fraction:
 
     return clampAngle(unclamped);
 }
-
-function resolveObjValue<T extends object>(keyframe: ResolvedKeyframe<T>, fraction: number, keys: ReadonlyArray<keyof T & string>): T {
-    const result: Partial<Record<keyof T, number>> = {};
-
-    const a = keyframe.curve.a as Record<keyof T, number>;
-    const b = keyframe.curve.b as Record<keyof T, number>;
-    const c = keyframe.curve.c as Record<keyof T, number>;
-    const val = keyframe.val as Record<keyof T, number>;
-
-    for (const key of keys) {
-        result[key] = a[key] * fraction * fraction * fraction
-            + b[key] * fraction * fraction
-            + c[key] * fraction
-            + val[key];
-    }
-
-    return result as T;
-}
-
-
-// Value resolving layer 2: numeric and object-property-looping implementations.
 
 function getCompletedFraction(startFrame: Keyframe<unknown>, endFrame: Keyframe<unknown>, currentTime: number) {
     const fraction = (currentTime - startFrame.time) / (endFrame.time - startFrame.time);
@@ -200,16 +137,13 @@ function getNumericValue(
         return frame2.val;
     }
 
+    const frame0 = keyframes[index2 - 2];
     const frame1 = keyframes[index2 - 1];
-
-    if (!frame1.curve) {
-        const frame0 = keyframes[index2 - 2];
-        const frame3 = keyframes[index2 + 1];
-        frame1.curve = determineCurve(frame0?.val, frame1.val, frame2.val, frame3?.val);
-    }
+    const frame3 = keyframes[index2 + 1];
+    const [ a, b, c ] = determineCurve(frame0?.val, frame1.val, frame2.val, frame3?.val);
 
     const fraction = getCompletedFraction(frame1, frame2, currentTime);
-    return resolveValue(frame1.curve.a, frame1.curve.b, frame1.curve.c, frame1.val, fraction);
+    return resolveValue(a, b, c, frame1.val, fraction);
 }
 
 function getObjectValue<T extends object>(keyframes: Keyframes<T>, currentTime: number, keys: ReadonlyArray<keyof T & string>): T {
@@ -227,18 +161,36 @@ function getObjectValue<T extends object>(keyframes: Keyframes<T>, currentTime: 
         return frame2.val;
     }
 
+    const frame0 = keyframes[index2 - 2];
     const frame1 = keyframes[index2 - 1];
+    const frame3 = keyframes[index2 + 1];
 
-    if (!frame1.curve) {
-        frame1.curve = determineCurveForObject<T>(keyframes, frame1, frame2, index2, keys);
+    const a = {} as Record<keyof T, number>;
+    const b = {} as Record<keyof T, number>;
+    const c = {} as Record<keyof T, number>;
+
+    const val0 = (frame0?.val ?? {}) as Record<keyof T, number>;
+    const val1 = frame1.val as Record<keyof T, number>;
+    const val2 = frame2.val as Record<keyof T, number>;
+    const val3 = (frame3?.val ?? {})  as Record<keyof T, number>;
+
+    for (const key of keys) {
+        const [keyA, keyB, keyC] = determineCurveParameters(val0[key], val1[key], val2[key], val3[key]);
+
+        a[key] = keyA;
+        b[key] = keyB;
+        c[key] = keyC;
     }
 
     const fraction = getCompletedFraction(frame1, frame2, currentTime);
-    return resolveObjValue<T>(frame1 as Required<typeof frame1>, fraction, keys);
+    const result = {} as Record<keyof T, number>;
+
+    for (const key of keys) {
+        result[key] = resolveNumberValue(a[key], b[key], c[key], frame1.val[key] as number, fraction);
+    }
+
+    return result as T;
 }
-
-
-// Value resolving layer 3: type-specific functions.
 
 export function getNumberValue(keyframes: Keyframes<number>, currentTime = getTime()): number {
     return getNumericValue(keyframes, currentTime, determineCurveParameters, resolveNumberValue);
