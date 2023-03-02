cd2016dda411d076bc410e187fa46b0f0fc5c5f3
FTWinston <ftwinston@gmail.com>
2023-03-02 22:46:26 +0000

    Fix position angle interpolation issue

--- src/utils/interpolate.ts
+++ src/utils/interpolate.ts
@@ -1,4 +1,4 @@
-import { clampAngle, Vector2D } from 'src/types/Vector2D';
+import { Vector2D } from 'src/types/Vector2D';
 import { Position } from 'src/types/Position';
 import { Keyframe, Keyframes } from 'src/types/Keyframes';
 import { getTime } from './timeSpans';
@@ -36,10 +36,7 @@ function distance(from: number, to: number) {
     return Math.max(distance, 0.00000001);
 }
 
-
-// Value resolving layer 1, part 1: determining curve parameters for a keypoint, based on values of surrounding keypoints.
-
-function determineCurveParameters(val0: number | undefined, val1: number, val2: number, val3: number | undefined): [number, number, number] {
+function resolveCurveValue(val0: number | undefined, val1: number, val2: number, val3: number | undefined, fraction: number): number {
     if (val0 === undefined) {
         val0 = val1;
     }
@@ -56,14 +53,14 @@ function determineCurveParameters(val0: number | undefined, val1: number, val2: 
     const m1 = val2 - val1 + t12 * ((val1 - val0) / t01 - (val2 - val0) / (t01 + t12));
     const m2 = val2 - val1 + t12 * ((val3 - val2) / t23 - (val3 - val1) / (t12 + t23));
 
-    return [
-        2 * (val1 - val2) + m1 + m2,
-        -3 * (val1 - val2) - m1 - m1 - m2,
-        m1,
-    ];
+    const a = 2 * (val1 - val2) + m1 + m2;
+    const b = -3 * (val1 - val2) - m1 - m1 - m2;
+    const c = m1;
+    
+    return resolveNumberValue(a, b, c, val1, fraction);
 }
 
-function determineCurveParametersForAngle(angle0: number | undefined, angle1: number, angle2: number, angle3: number | undefined) {
+function resolveCurveValueForAngle(angle0: number | undefined, angle1: number, angle2: number, angle3: number | undefined, fraction: number) {
     if (angle3 !== undefined) {
         while (angle3 - angle2 > Math.PI) {
             angle3 -= Math.PI * 2;
@@ -89,7 +86,7 @@ function determineCurveParametersForAngle(angle0: number | undefined, angle1: nu
         }
     }
 
-    return determineCurveParameters(angle0, angle1, angle2, angle3);
+    return resolveCurveValue(angle0, angle1, angle2, angle3, fraction);
 }
 
 function resolveNumberValue(a: number, b: number, c: number, d: number, fraction: number) {
@@ -97,12 +94,6 @@ function resolveNumberValue(a: number, b: number, c: number, d: number, fraction
          + b * fraction * fraction
          + c * fraction
          + d;
-}
-
-function resolveAngleValue(a: number, b: number, c: number, d: number, fraction: number) {
-    const unclamped = resolveNumberValue(a, b, c, d, fraction);
-
-    return clampAngle(unclamped);
 }
 
 function getCompletedFraction(startFrame: Keyframe<unknown>, endFrame: Keyframe<unknown>, currentTime: number) {
@@ -114,8 +105,7 @@ function getCompletedFraction(startFrame: Keyframe<unknown>, endFrame: Keyframe<
 function interpolateNumeric(
     keyframes: Keyframes<number>,
     currentTime: number,
-    determineCurve: typeof determineCurveParameters,
-    resolveValue: typeof resolveNumberValue,
+    resolveValue: typeof resolveCurveValue,
 ): number {
     const index2 = getFirstFutureIndex(keyframes, currentTime);
 
@@ -134,13 +124,14 @@ function interpolateNumeric(
     const frame0 = keyframes[index2 - 2];
     const frame1 = keyframes[index2 - 1];
     const frame3 = keyframes[index2 + 1];
-    const [ a, b, c ] = determineCurve(frame0?.val, frame1.val, frame2.val, frame3?.val);
-
     const fraction = getCompletedFraction(frame1, frame2, currentTime);
-    return resolveValue(a, b, c, frame1.val, fraction);
+
+    return resolveValue(frame0?.val, frame1.val, frame2.val, frame3?.val, fraction);
 }
 
-function interpolateObjectKeys<T extends object>(keyframes: Keyframes<T>, currentTime: number, keys: ReadonlyArray<keyof T & string>): T {
+type KeyCurveResolution<T> = Readonly<Record<keyof T, typeof resolveCurveValue>>;
+
+function interpolateObjectKeys<T extends object>(keyframes: Keyframes<T>, currentTime: number, keys: KeyCurveResolution<T>): T {
     const index2 = getFirstFutureIndex(keyframes, currentTime);
 
     if (index2 === -1) {
@@ -159,48 +150,45 @@ function interpolateObjectKeys<T extends object>(keyframes: Keyframes<T>, curren
     const frame1 = keyframes[index2 - 1];
     const frame3 = keyframes[index2 + 1];
 
-    const a = {} as Record<keyof T, number>;
-    const b = {} as Record<keyof T, number>;
-    const c = {} as Record<keyof T, number>;
-
     const val0 = (frame0?.val ?? {}) as Record<keyof T, number>;
     const val1 = frame1.val as Record<keyof T, number>;
     const val2 = frame2.val as Record<keyof T, number>;
     const val3 = (frame3?.val ?? {})  as Record<keyof T, number>;
 
-    for (const key of keys) {
-        const [keyA, keyB, keyC] = determineCurveParameters(val0[key], val1[key], val2[key], val3[key]);
-
-        a[key] = keyA;
-        b[key] = keyB;
-        c[key] = keyC;
-    }
-
     const fraction = getCompletedFraction(frame1, frame2, currentTime);
     const result = {} as Record<keyof T, number>;
 
-    for (const key of keys) {
-        result[key] = resolveNumberValue(a[key], b[key], c[key], frame1.val[key] as number, fraction);
+    for (const [keyName, resolveValue] of Object.entries<typeof resolveCurveValue>(keys)) {
+        const key = keyName as keyof T;
+        result[key] = resolveValue(val0[key], val1[key], val2[key], val3[key], fraction);
     }
 
     return result as T;
 }
 
 export function interpolateNumber(keyframes: Keyframes<number>, currentTime = getTime()): number {
-    return interpolateNumeric(keyframes, currentTime, determineCurveParameters, resolveNumberValue);
+    return interpolateNumeric(keyframes, currentTime, resolveCurveValue);
 }
 
 export function interpolateAngle(keyframes: Keyframes<number>, currentTime = getTime()): number {
-    return interpolateNumeric(keyframes, currentTime, determineCurveParametersForAngle, resolveAngleValue);
+    return interpolateNumeric(keyframes, currentTime, resolveCurveValueForAngle);
 }
 
-const vectorKeys: ReadonlyArray<keyof Vector2D> = ['x', 'y'];
+const vectorKeys: KeyCurveResolution<Vector2D> = {
+    x: resolveCurveValue,
+    y: resolveCurveValue,
+};
 
 export function interpolateVector(keyframes: Keyframes<Vector2D>, currentTime = getTime()): Vector2D {
     return interpolateObjectKeys<Vector2D>(keyframes, currentTime, vectorKeys);
 }
 
-const positionKeys: ReadonlyArray<keyof Position> = ['x', 'y', 'angle', 'evade'];
+const positionKeys: KeyCurveResolution<Position> = {
+    x: resolveCurveValue,
+    y: resolveCurveValue,
+    angle: resolveCurveValueForAngle,
+    evade: resolveCurveValue,
+};
 
 export function interpolatePosition(keyframes: Keyframes<Position>, currentTime = getTime()): Position {
     return interpolateObjectKeys<Position>(keyframes, currentTime, positionKeys);
