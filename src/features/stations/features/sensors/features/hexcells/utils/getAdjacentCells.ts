384b7a8f72af280733d15ef352642969cd9e5894
FTWinston <ftwinston@gmail.com>
2023-04-02 23:11:55 +0100

    Now calculating adjacent cells

--- src/features/stations/features/sensors/features/hexcells/utils/getAdjacentCells.ts
+++ src/features/stations/features/sensors/features/hexcells/utils/getAdjacentCells.ts
@@ -1,4 +1,63 @@
+interface Coordinate {
+    row: number;
+    col: number;
+}
+
+function coordinateFromIndex(index: number, columns: number): Coordinate {
+    const col = index % columns;
+    const row = Math.floor(index / columns);
+
+    return { row, col };
+}
+
+function indexFromCoordinate(coordinate: Coordinate, columns: number): number {
+    return coordinate.row * columns + coordinate.col;
+}
+
 export function getAdjacentCells(index: number, columns: number, rows: number): number[] {
-    // TODO: implement this!
-    return [1, 2, 3];
+    const { row, col } = coordinateFromIndex(index, columns);
+
+    const adjacentCoords: Coordinate[] = [];
+
+    if (row > 0) {
+        adjacentCoords.push({ row: row - 1, col });
+    }
+    if (row < rows - 1) {
+        adjacentCoords.push({ row: row + 1, col });
+    }
+
+    if (col % 2 === 0) {
+        // Even columns are shifted down.
+        if (col > 0) {
+            adjacentCoords.push({ row, col: col - 1 });
+            if (row < rows - 1) {
+                adjacentCoords.push({ row: row + 1, col: col - 1 })
+            }
+        }
+        if (col < columns - 1) {
+            adjacentCoords.push({ row, col: col + 1 });
+            if (row < rows - 1) {
+                adjacentCoords.push({ row: row + 1, col: col + 1 })
+            }
+        }
+    }
+    else {
+        // Odd columns are shifted up.
+        if (col > 0) {
+            adjacentCoords.push({ row, col: col - 1 })
+            
+            if (row > 0) {
+                adjacentCoords.push({ row: row - 1, col: col - 1 });
+            }
+        }
+        if (col < columns - 1) {
+            adjacentCoords.push({ row, col: col + 1 })
+            if (row > 0) {
+                adjacentCoords.push({ row: row - 1, col: col + 1 });
+            }
+        }
+    }
+
+    return adjacentCoords
+        .map(coord => indexFromCoordinate(coord, columns));
 }
