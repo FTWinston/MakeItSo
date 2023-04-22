35b89b4b9403b08c707cd29947cdbf95c45e1c04
FTWinston <ftwinston@gmail.com>
2023-04-22 23:36:35 +0100

    Split board shape generation into its own file

--- src/features/stations/features/sensors/features/hexcells/utils/generateBoard.ts
+++ src/features/stations/features/sensors/features/hexcells/utils/generateBoard.ts
@@ -2,68 +2,7 @@ import { getRandomInt } from 'src/utils/random';
 import type { CellBoardDefinition } from '../types/CellBoard';
 import { CellState, CellType, CountType, UnderlyingCellState } from '../types/CellState';
 import { getAdjacentCells, getCellsInRadius } from './getAdjacentCells';
-
-/** Decide bounds based on number of cells. */
-function determineSize(landscapeOrientation: boolean, numCells: number, numGaps: number) {
-    const totalCells = numCells + numGaps;
-    
-    let rows = Math.sqrt(totalCells);
-    rows *= Math.random() * 0.3 + 0.85;
-    rows = Math.ceil(rows);
-
-    let columns = Math.floor(totalCells / rows);
-
-    if (landscapeOrientation) {
-        const tmp = rows;
-        rows = columns;
-        columns = tmp;
-    }
-
-    return { rows, columns };
-}
-
-/** Replace random cells with null, but do so symmetrically, either rotationally or mirrored. */
-function placeNulls(board: Array<UnderlyingCellState | null>, rows: number, columns: number, numToAssign: number) {
-    let primaryIndexes: number[] = [];
-    let mirroredIndexes: number[] = [];
-
-    if (columns % 2 === 0) {
-        // Rotational mirror
-        const middleIndex = Math.ceil(board.length / 2);
-        primaryIndexes = Array(middleIndex + 1).fill(0).map((_, i) => i);
-        mirroredIndexes = Array(middleIndex + 1).fill(0).map((_, i) => board.length - i - 1);
-    }
-    else {
-        // Horizontal reflection mirror
-        const middlestColumn = Math.ceil(columns / 2);
-        const getIndex = (row: number, col: number) => row * columns + col;
-
-        for (let row = 0; row <= rows; row++) {
-            for (let col = 0; col <= middlestColumn; col++) {
-                primaryIndexes.push(getIndex(row, col));
-                mirroredIndexes.push(getIndex(row, columns - col - 1));
-            }
-        }
-    }
-    
-    for (numToAssign; numToAssign > 0; numToAssign--) {
-        let boardIndex: number;
-        let indexIndex: number;
-
-        do {
-            indexIndex = getRandomInt(primaryIndexes.length);
-            boardIndex = primaryIndexes[indexIndex];
-        } while (board[boardIndex] === null);
-
-        board[boardIndex] = null;
-
-        let mirroredBoardIndex = mirroredIndexes[indexIndex];
-        if (board[mirroredBoardIndex] !== null && numToAssign > 0) {
-            board[mirroredBoardIndex] = null;
-            numToAssign--;
-        }
-    }
-}
+import { ShapeConfig, generateBoardShape } from './generateBoardShape';
 
 /** Replace random cells with copies of the given template. */
 function assignCells(board: Array<UnderlyingCellState | null>, numToAssign: number, assignTemplate: UnderlyingCellState) {
@@ -157,15 +96,7 @@ function setCounts(
     }
 }
 
-export interface GenerationConfig {
-    orientation?: 'portrait' | 'landscape';
-
-    /** Number of cells in the grid. */
-    numCells: number;
-
-    /** Fraction of the grid that contains spaces instead of cells. Lower values are easier. */
-    gapFraction: number;
-
+export interface GenerationConfig extends ShapeConfig {
     /** Fraction of valid cells that are bombs. Lower values are easier. */
     bombFraction: number;
 
@@ -181,10 +112,10 @@ export interface GenerationConfig {
     /** Fraction of rows (along any axis) that get a row clue. Higher values are easier. */
     rowClueFraction?: number;
 
-    // TODO: use this
+    /** Chance of using a contiguous clue when a cell has contiguous bombs. */
     contiguousClueFraction?: number;
 
-    // TODO: use this
+    /** Chance of using a split clue when a cell has non-contiguous bombs. */
     splitClueFraction?: number;
 }
 
@@ -196,24 +127,10 @@ function mustStartedRevealed(cell: UnderlyingCellState | null) {
         || cell.type === CellType.IndicatorTRBL;
 }
 
-export function generateBoard(config: GenerationConfig): CellBoardDefinition {
-    const { rows, columns } = determineSize(config.orientation === 'landscape', config.numCells, Math.round(config.gapFraction * config.numCells));
+export function generateBoard(config: GenerationConfig): CellBoardDefinition { 
+    const { rows, columns, cells: underlying } = generateBoardShape(config);
 
-    const underlying: Array<UnderlyingCellState | null> = new Array(rows * columns)
-        .fill(0)
-        .map(() => ({
-            type: CellType.Empty,
-            countType: CountType.Normal,
-            number: 0,
-        }));
-    
     let numNormalCells = config.numCells;
-    
-    const numToRemove = underlying.length - config.numCells
-    if (numToRemove > 0) {
-        placeNulls(underlying, rows, columns, numToRemove);
-        numNormalCells -= numToRemove;
-    }
     
     if (config.bombFraction && config.bombFraction > 0) {
         const numBombCells = Math.round(numNormalCells * config.bombFraction);
---
+++ src/features/stations/features/sensors/features/hexcells/utils/generateBoardShape.ts
@@ -0,0 +1,93 @@
+import { getRandomInt } from 'src/utils/random';
+import { CellType, CountType, UnderlyingCellState } from '../types/CellState';
+
+export interface ShapeConfig {
+    orientation?: 'portrait' | 'landscape';
+
+    /** Number of cells in the grid. */
+    numCells: number;
+
+    /** Fraction of the grid that contains spaces instead of cells. Lower values are easier. */
+    gapFraction: number;
+}
+
+/** Decide bounds based on number of cells. */
+function determineSize(landscapeOrientation: boolean, numCells: number, numGaps: number) {
+    const totalCells = numCells + numGaps;
+    
+    let rows = Math.sqrt(totalCells);
+    rows *= Math.random() * 0.3 + 0.85;
+    rows = Math.ceil(rows);
+
+    let columns = Math.floor(totalCells / rows);
+
+    if (landscapeOrientation) {
+        const tmp = rows;
+        rows = columns;
+        columns = tmp;
+    }
+
+    return { rows, columns };
+}
+
+/** Replace random cells with null, but do so symmetrically, either rotationally or mirrored. */
+function placeNulls(board: Array<UnderlyingCellState | null>, rows: number, columns: number, numToAssign: number) {
+    let primaryIndexes: number[] = [];
+    let mirroredIndexes: number[] = [];
+
+    if (columns % 2 === 0) {
+        // Rotational mirror
+        const middleIndex = Math.ceil(board.length / 2);
+        primaryIndexes = Array(middleIndex + 1).fill(0).map((_, i) => i);
+        mirroredIndexes = Array(middleIndex + 1).fill(0).map((_, i) => board.length - i - 1);
+    }
+    else {
+        // Horizontal reflection mirror
+        const middlestColumn = Math.ceil(columns / 2);
+        const getIndex = (row: number, col: number) => row * columns + col;
+
+        for (let row = 0; row <= rows; row++) {
+            for (let col = 0; col <= middlestColumn; col++) {
+                primaryIndexes.push(getIndex(row, col));
+                mirroredIndexes.push(getIndex(row, columns - col - 1));
+            }
+        }
+    }
+    
+    for (numToAssign; numToAssign > 0; numToAssign--) {
+        let boardIndex: number;
+        let indexIndex: number;
+
+        do {
+            indexIndex = getRandomInt(primaryIndexes.length);
+            boardIndex = primaryIndexes[indexIndex];
+        } while (board[boardIndex] === null);
+
+        board[boardIndex] = null;
+
+        let mirroredBoardIndex = mirroredIndexes[indexIndex];
+        if (board[mirroredBoardIndex] !== null && numToAssign > 0) {
+            board[mirroredBoardIndex] = null;
+            numToAssign--;
+        }
+    }
+}
+
+export function generateBoardShape(config: ShapeConfig) {
+    const { rows, columns } = determineSize(config.orientation === 'landscape', config.numCells, Math.round(config.gapFraction * config.numCells));
+
+    const cells: Array<UnderlyingCellState | null> = new Array(rows * columns)
+        .fill(0)
+        .map(() => ({
+            type: CellType.Empty,
+            countType: CountType.Normal,
+            number: 0,
+        }));
+    
+    const numToRemove = cells.length - config.numCells
+    if (numToRemove > 0) {
+        placeNulls(cells, rows, columns, numToRemove);
+    }
+
+    return { rows, columns, cells };
+}
