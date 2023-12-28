7644468e81bd19bd9b3f58440b23b7b2444a7a3c
FTWinston <ftwinston@gmail.com>
2023-12-28 23:22:04 +0000

    Standardized Dialog component

--- src/features/stations/features/sensors/features/scanselect/components/ScanItemDialog.tsx
+++ src/features/stations/features/sensors/features/scanselect/components/ScanItemDialog.tsx
@@ -1,9 +1,7 @@
 import { PropsWithChildren } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, Dialog, DialogTitle, IconButton, styled } from 'src/lib/mui';
+import { Button, Dialog } from 'src/lib/mui';
 import { ScanItemId } from '../types/ScanItemId';
-import { DialogContent } from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
 
 export type ItemStatus = 'active' | 'inactive' | 'available' | 'unavailable';
 
@@ -13,47 +11,20 @@ interface Props {
     onConfirm?: () => void;
 }
 
-const Root = styled(Dialog)({
-    
-});
-
-const Title = styled(DialogTitle)(({ theme }) => ({
-    paddingRight: '3em',
-}))
-
-
 export const ScanItemDialog: React.FC<PropsWithChildren<Props>> = props => {
     const { t } = useTranslation('sensors');
     const title = props.itemId ? t(`scan ${props.itemId}`) : '';
 
     return (
-        <Root
+        <Dialog
             open={props.itemId !== undefined}
             onClose={props.onClose}
-            aria-labelledby="scan-item-dialog-title"
+            title={title}
+            actions={<Button color="primary" onClick={props.onConfirm}>
+                Confirm
+            </Button>}
         >
-            <Title id="scan-item-dialog-title">
-                {title}
-            </Title>
-
-            <IconButton
-                aria-label="close"
-                onClick={props.onClose}
-                sx={{
-                    position: 'absolute',
-                    right: 8,
-                    top: 8,
-                    color: (theme) => theme.palette.grey[500],
-                }}
-                >
-                <CloseIcon />
-            </IconButton>
             
-            <DialogContent>
-                <Button color="primary" onClick={props.onConfirm}>
-                    Confirm
-                </Button>
-            </DialogContent>
-        </Root>
+        </Dialog>
     );
 }
--- src/lib/mui/Dialog.tsx
+++ src/lib/mui/Dialog.tsx
@@ -1,16 +1,82 @@
-import MuiDialog from '@mui/material/Dialog';
+import MuiDialog, { DialogProps } from '@mui/material/Dialog';
+import MuiDialogActions from '@mui/material/DialogActions';
 import MuiDialogContent from '@mui/material/DialogContent';
 import MuiDialogTitle from '@mui/material/DialogTitle';
 import { styled } from '@mui/material/styles';
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton } from './IconButton';
 
-export const Dialog = styled(MuiDialog)({  
-    
-}) as typeof MuiDialog;
+const DialogTitle = styled(MuiDialogTitle)({
+    padding: '0.5em 3em 0.5em 1em',
+});
 
-export const DialogTitle = styled(MuiDialogTitle)({
-    padding: '0.5em 1em',
-}) as typeof MuiDialogTitle;
+const DialogContent = styled(MuiDialogContent)({
+    padding: '0.5em 1.25em',
+});
 
-export const DialogContent = styled(MuiDialogContent)({
-    padding: '1.25em 1.5em',
-}) as typeof MuiDialogContent;
+const DialogActions = styled(MuiDialogActions)({
+    padding: '0.5em',
+});
+
+const titleId = 'dialog-title'
+
+type Props = DialogProps & {
+    actions?: JSX.Element;
+}
+
+export const Dialog: React.FC<Props> = props => {
+    const onClose = props.onClose;
+
+    const closeButton = onClose
+        ? (
+            <IconButton
+                aria-label="close"
+                onClick={onClose as () => {}}
+                sx={{
+                    position: 'absolute',
+                    right: 8,
+                    top: 8,
+                    color: (theme) => theme.palette.grey[500],
+                }}
+                >
+                <CloseIcon />
+            </IconButton>
+        ) : undefined;
+
+    const title = props.title
+        ? (
+            <DialogTitle id={titleId}>
+                {props.title}
+            </DialogTitle>
+        )
+        : undefined;
+
+    const content = props.children
+        ? (
+            <DialogContent>
+                {props.children}
+            </DialogContent>
+        )
+        : undefined;
+
+    const actions = props.actions
+        ? (
+            <DialogActions>
+                {props.actions}
+            </DialogActions>
+        )
+        : undefined;
+
+    return (
+        <MuiDialog
+            open={props.open}
+            onClose={props.onClose}
+            aria-labelledby={title ? titleId : undefined}
+        >
+            {title}
+            {closeButton}
+            {content}
+            {actions}
+        </MuiDialog>
+    );
+}
